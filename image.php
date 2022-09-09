71ce22455424b83d9185da1536c6207566bb47c4
Okawa <hidehide21@gmail.com>
2022-09-09 22:50:16 +0900

    ファイル整理

--- image.php
+++ image.php
@@ -1,26 +1,22 @@
 <?php
-function mb_wordwrap($str, $width = 15, $break = PHP_EOL, $encode = "UTF-8")
-{
-    $c = mb_strlen($str, $encode);
-    $c > 50 ? $c = 50 : "";
-    $arr = [];
-    for ($i = 0; $i <= $c; $i += $width) {
-        $arr[] = mb_substr($str, $i, $width, $encode);
-    }
-    // return implode($break, $arr);
-    return  $arr;
-}
-$text = [];
-if (isset($_GET["text"])) {
-    $text  = mb_wordwrap($_GET["text"]);
-}
-// var_dump($text);
-// echo $text;
 ini_set('display_errors', "On");
 //ヘッダーの設定
 header("Content-type: image/jpeg");
 
-
+function mb_wordwrap($str, $width = 15, $break = PHP_EOL, $encode = "UTF-8")
+{
+    $count = mb_strlen($str, $encode);
+    $count > 45 ? $count = 45 : "";
+    $array = [];
+    for ($i = 0; $i <= $count; $i += $width) {
+        array_push($array, mb_substr($str, $i, $width, $encode));
+    }
+    return  $array;
+}
+$textArray = [];
+if (isset($_GET["text"])) {
+    $textArray  = mb_wordwrap($_GET["text"]);
+}
 
 // 文字列を挿入する先の画像
 $file = "./sample.jpg";
@@ -31,19 +27,13 @@ $newfile = "sample_maoji.jpg";
 // コピー先画像作成
 $image = imagecreatefromjpeg($file);
 
-// 挿入する文字列
-// $text = wordwrap($origin_text, 5, ',', true);
-// $text = ["ああああ", "いいいいいい", "ええええええ"];
-
-// 挿入する文字列のフォント(今回はWindowsに入ってたメイリオを使う)
-// $fontfile = "C:\Windows\Fonts\meiryo.ttc";
-$fontfile = dirname(__FILE__) . "/font/NotoSansJP-Medium.otf";
+$fontFile = dirname(__FILE__) . "/font/NotoSansJP-Medium.otf";
 
 // 挿入する文字列の色(白)
 $color = imagecolorallocate($image, 255, 255, 255);
 
 // 挿入する文字列のサイズ(ピクセル)
-$fontsize = 20;
+$fontSize = 22;
 $margin = 10;
 
 //背景用画像のサイズを取得する
@@ -52,25 +42,25 @@ $filew = $filesize[0];
 $fileh = $filesize[1];
 
 //テキストが合計何行になるかによって開始位置を設定（上下中央に持ってくるため）
-$count = count($text); //行数を計算
-$height = ($fontsize + $margin) * $count; //テキスト部分の高さを計算
+$count = count($textArray); //行数を計算
+$height = ($fontSize + $margin) * $count; //テキスト部分の高さを計算
 
 //テキスト開始位置。-10はbaseline分ですが、結果を見ながら目分量で調整する必要がありそう。
 $y = (($fileh - $height) / 2);
 
 //文字一行ずつ出力していく
-foreach ($text as $val) {
+foreach ($textArray as $text) {
     //文字エリアの座標を取得する関数です
-    $pos = imagettfbbox($fontsize, 0, $fontfile, $val);
+    $pos = imagettfbbox($fontSize, 0, $fontFile, $text);
 
     //左右中央に持ってくるため、(画像幅-文字幅)/2を開始位置とします。
     $x = ($filew - ($pos[4] - $pos[6])) / 2;
 
     //imagettftextで実際に文字を生成する
-    imagettftext($image, $fontsize, 0, $x, $y, $color, $fontfile, $val);
+    imagettftext($image, $fontSize, 0, $x, $y, $color, $fontFile, $text);
 
     //縦方向の位置を文字サイズ＋行間分ずらす（これでline-height:2ぐらいになるはず）
-    $y = $y - ($pos[7] - $pos[1]) + $margin + $fontsize;
+    $y = $y - ($pos[7] - $pos[1]) + $margin + $fontSize;
 }
 
 // // 挿入する文字列の角度
--- test.js
+++
@@ -1,7 +0,0 @@
-array = [0, 1, 2, 3];
-
-const ttt = [array.map((item) => item), 12];
-const ttt2 = [...array.map((item) => item), 12];
-
-console.log(ttt);
-console.log(ttt2);
